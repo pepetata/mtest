4d16b214411a098f379d868eaaf3080e20024b55
Flavio <flavio_luiz_ferreira@hotmail.com>
2018-01-02 15:27:16 -0200

    c

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var port = process.env.PORT || 8080
-//var config = {
-//   host: "localhost",
-//   user: process.env.SQL_USER ? process.env.SQL_USER : "mb",
-//   password: process.env.SQL_PASSWORD ? process.env.SQL_PASSWORD : "mb321.123",
-//   database: process.env.SQL_DATABASE ? process.env.SQL_DATABASE : "mb",
-//   timezone: "-02:00"
-////   , debug: true
-//};
+var config = {
+   host: "localhost",
+   user: process.env.SQL_USER ? process.env.SQL_USER : "mb",
+   password: process.env.SQL_PASSWORD ? process.env.SQL_PASSWORD : "mb321.123",
+   database: process.env.SQL_DATABASE ? process.env.SQL_DATABASE : "mb",
+   timezone: "-02:00"
+//   , debug: true
+};
 //console.log('process.env.INSTANCE_CONNECTION_NAME', process.env.INSTANCE_CONNECTION_NAME);
 //console.log('process.env.NODE_ENV',process.env.NODE_ENV);
 //if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
@@ -23,8 +23,6 @@ var express = require('express')
         , socketIO = require('socket.io')
         , server = http.createServer(app)
         , io = socketIO.listen(server)
-        , bodyParser = require('body-parser')
-        , stylus = require('stylus')
         , mysql = require('mysql')
         ;
 
@@ -39,16 +37,23 @@ server.listen(port,  function () {
    console.log('Server is running on localhost:' + port + '...', server.address());
 });
 
-//var pool = mysql.createPool(config);
+var pool = mysql.createPool(config);
 
 app.set('views', process.cwd() + '/public/views');
 app.set('view engine', 'stylus');
-app.use(express.static("public"));
-app.use(stylus.middleware(process.cwd() + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use('/public', express.static(process.cwd() + '/public'));
+//app.use(express.static("/"));
+//app.use(stylus.middleware(process.cwd() + '/'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/static', express.static('/'))
 
+app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + "/index.html");
+});
+app.get('/index.html', function(req, res) {
+    res.sendFile(process.cwd() + "/index.html");
+});
 
 //pool.getConnection(function (err, connection) {
 //   if (err)
--- server.js
+++ server.js
@@ -1,9 +1,108 @@
 #!/usr/bin/env node
 
-var io = require(process.cwd() + '/app.js').io;
-var routesSocket = require(process.cwd() + '/routesSocket.js')
+
+var port = process.env.PORT || 8080
+var config = {
+   host: "localhost",
+   user: process.env.SQL_USER ? process.env.SQL_USER : "mb",
+   password: process.env.SQL_PASSWORD ? process.env.SQL_PASSWORD : "mb321.123",
+   database: process.env.SQL_DATABASE ? process.env.SQL_DATABASE : "mb",
+   timezone: "-02:00"
+//   , debug: true
+};
+//console.log('process.env.INSTANCE_CONNECTION_NAME', process.env.INSTANCE_CONNECTION_NAME);
+//console.log('process.env.NODE_ENV',process.env.NODE_ENV);
+//if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
+//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+//}
+//
+//console.log('config=',config);
+
+var express = require('express')
+        , app = express()
+        , http = require('http')
+        , socketIO = require('socket.io')
+        , server = http.createServer(app)
+        , io = socketIO.listen(server)
+        , mysql = require('mysql')
+        ;
+
+io.mbList = [];
+io.custList = [];
 
 
-routesSocket();
+// Starting Express server
+//TODO - eliminate IP
+// server.listen(port, function() {
+server.listen(port, function () {
+   console.log('Server is running on localhost:' + port + '...', server.address());
+});
+
+var pool = mysql.createPool(config);
+
+app.set('views', process.cwd() + '/public/views');
+app.set('view engine', 'stylus');
+//app.use(express.static("/"));
+//app.use(stylus.middleware(process.cwd() + '/'));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: true}));
+//app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/static', express.static('/'))
+
+app.get('/', function (req, res) {
+   res.sendFile(process.cwd() + "/index.html");
+});
+app.get('/index.html', function (req, res) {
+   res.sendFile(process.cwd() + "/index.html");
+});
+
+//pool.getConnection(function (err, connection) {
+//   if (err)
+//      throw err;
+//   console.log("Connected to database!");
+//   routes(app, connection);
+//
+//});
+//
+//var sql = "SELECT * FROM  `motoboy`";
+//pool.query(sql, [], function (error, resultCont, fields) {
+//   if (error) {
+//      console.log(error);
+//      return;
+//   }
+//});
+//
+
+//var io = require(process.cwd() + '/app.js').io;
+//var routesSocket = require(process.cwd() + '/routesSocket.js')
+
+
+
+
+var connections = [];
+io.on('connection', function (client) {
+   connections.push(client);
+   console.log('Connected: %s. Total: %s', client.id, connections.length);
+
+   var count = 0;
+
+   console.log("enviando pingx");
+   client.emit('pingx', {date: Date.now()}, function () {
+      count = 0;
+   });
+
+   client.on('pongx', function (data) {
+      console.log('recebeu um pongx do', data.id, client.id, data.lat, data.lng);
+   });
+
+   setInterval(function () {
+      console.log("enviando pingx interval");
+      client.emit('pingx', {date: Date.now()}, function () {
+         count = 0;
+      });
+   }, 10000);
+
+});
+
 
 module.exports = {io: io};
