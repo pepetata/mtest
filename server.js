c81364b7b851808b483f5307b2883657daa67356
Flavio <flavio_luiz_ferreira@hotmail.com>
2018-01-02 21:56:54 -0200

    c

--- server.js
+++ server.js
@@ -4,15 +4,15 @@
 var port = process.env.PORT || 8080
 var config = {
    host: process.env.OPENSHIFT_MYSQL_DB_HOST ? process.env.OPENSHIFT_MYSQL_DB_HOST : "localhost",
-   port: process.env.OPENSHIFT_MYSQL_DB_PORT ? process.env.OPENSHIFT_MYSQL_DB_PORT : "3306",
+   port: process.env.OPENSHIFT_MYSQL_DB_PORT ? process.env.OPENSHIFT_MYSQL_DB_PORT : "13306",
    user: process.env.OPENSHIFT_MYSQL_DB_USERNAME ? process.env.OPENSHIFT_MYSQL_DB_USERNAME : "mb",
    password: process.env.OPENSHIFT_MYSQL_DB_PASSWORD ? process.env.OPENSHIFT_MYSQL_DB_PASSWORD : "mb321.123",
    database: process.env.OPENSHIFT_APP_NAME ? process.env.OPENSHIFT_APP_NAME : "mb",
    timezone: "-02:00"
-//   , debug: true
+   , debug: true
 };
 
-console.log(config);
+console.log(config,process.env.OPENSHIFT_MYSQL_DB_PORT);
 
 var express = require('express')
         , app = express()
